d45ec3d6143f260e79171bc7eeb133f08d0f64d5
ZRunner <z.runner.mc@gmail.com>
2025-02-27 17:29:21 +0100

    fix(reviews): SVG icon attributes in edit.js

--- src/blocks/customer-reviews/edit.js
+++ src/blocks/customer-reviews/edit.js
@@ -92,7 +92,7 @@ export default function Edit({ attributes, setAttributes }) {
                     Explorez nos avis
                     <svg width="25" height="24" viewBox="0 0 25 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M9.41016 19.9201L15.9302 13.4001C16.7002 12.6301 16.7002 11.3701 15.9302 10.6001L9.41016 4.08008"
-                            stroke="currentColor" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
+                            stroke="currentColor" strokeWidth="2" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round" />
                     </svg>
                 </span>
                 <div className="swiper">
