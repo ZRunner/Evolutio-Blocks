39a6be79b311b73f64d43c63fcf890c19924a44e
ZRunner <z.runner.mc@gmail.com>
2025-02-27 15:58:21 +0100

    feat(reviews): truncate content + add "Read more" link

--- src/blocks/customer-reviews/render.php
+++ src/blocks/customer-reviews/render.php
@@ -24,6 +24,14 @@ $reviews = new WP_Query(array(
         <?php echo $description; ?>
     </p>
 
+    <a href="/avis" class="evolutio-customer-reviews__readmore">
+        Explorez nos avis
+        <svg width="25" height="24" viewBox="0 0 25 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M9.41016 19.9201L15.9302 13.4001C16.7002 12.6301 16.7002 11.3701 15.9302 10.6001L9.41016 4.08008"
+                stroke="currentColor" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
+        </svg>
+    </a>
+
     <div class="swiper">
         <div class="swiper-wrapper">
             <?php if ($reviews->have_posts()) :
