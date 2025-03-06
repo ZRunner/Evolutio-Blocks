a01e3d9881b0e60e4ccb1208e77c537434a1a186
ZRunner <z.runner.mc@gmail.com>
2025-03-06 11:20:33 +0100

    feat(richtext): add max width support

--- src/blocks/richtext/edit.js
+++ src/blocks/richtext/edit.js
@@ -1,6 +1,11 @@
 //@ts-check
 
-import { useBlockProps, RichText } from '@wordpress/block-editor';
+import { useBlockProps, RichText, InspectorControls } from '@wordpress/block-editor';
+import { PanelRow } from '@wordpress/components';
+import { PanelBody } from '@wordpress/components';
+import { __experimentalUnitControl as UnitControl } from '@wordpress/components';
+import { Fragment } from 'react';
+
 
 /**
  * @type {{
@@ -19,11 +24,12 @@ const DISABLED_FORMATS = [
  * @argument {import('@wordpress/blocks').BlockEditProps<
 * 	{
 * 		text: string,
+*       maxWidth: string,
 *	}>} props
 * @return {import('react').ReactElement} Element to render.
 */
 export default function Edit({ attributes, setAttributes }) {
-    const { text } = attributes;
+    const { text, maxWidth } = attributes;
 
     /** @type {{name: string}[]} */
     const defaultFormats = wp.data.select('core/rich-text').getFormatTypes();
@@ -36,14 +42,37 @@ export default function Edit({ attributes, setAttributes }) {
         setAttributes({ text: newText });
     }
 
+    /**
+     * @param {string} newMaxWidth The new block max width
+     */
+    const updateMaxWidth = (newMaxWidth) => {
+        setAttributes({ maxWidth: newMaxWidth });
+    }
+
     return (
-        <RichText
-            {...useBlockProps({ className: "evolutio-richtext" })}
-            tagName="div"
-            allowedFormats={allowedFormats}
-            value={text}
-            onChange={updateDescription}
-            placeholder="Write your text here"
-        />
+        <Fragment>
+            <InspectorControls>
+                <PanelBody title="RichText Settings" initialOpen>
+                    <PanelRow>
+                        <UnitControl
+                            label="Max width"
+                            value={maxWidth}
+                            onChange={updateMaxWidth}
+                            __next40pxDefaultSize
+                        />
+                    </PanelRow>
+                </PanelBody>
+            </InspectorControls>
+            <div {...useBlockProps({ className: "evolutio-richtext" })}>
+                <RichText
+                    style={{ maxWidth: maxWidth, letterSpacing: "0.02px" }}
+                    tagName="div"
+                    allowedFormats={allowedFormats}
+                    value={text}
+                    onChange={updateDescription}
+                    placeholder="Write your text here"
+                />
+            </div>
+        </Fragment>
     );
 }
--- src/blocks/richtext/save.js
+++ src/blocks/richtext/save.js
@@ -5,17 +5,20 @@ import { RichText, useBlockProps } from '@wordpress/block-editor';
  * @argument {import('@wordpress/blocks').BlockEditProps<
 *  {
     * 		text: string,
+    *       maxWidth: string,
 *  }>} props
 * @return {import('react').ReactElement} Element to render.
 */
 export default function Save({ attributes }) {
-    const { text } = attributes;
+    const { text, maxWidth } = attributes;
 
     return (
-        <RichText.Content
-            {...useBlockProps.save({ className: "evolutio-richtext" })}
-            tagName="div"
-            value={text}
-        />
+        <div {...useBlockProps.save({ className: "evolutio-richtext" })}>
+            <RichText.Content
+                style={{ maxWidth, letterSpacing: "0.02px" }}
+                tagName="div"
+                value={text}
+            />
+        </div>
     )
 }
