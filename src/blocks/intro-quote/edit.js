5623fa0c75b68d6f5d7d887996ff529d00777c53
ZRunner <z.runner.mc@gmail.com>
2025-08-02 16:24:10 +0200

    feat(intro-quote): remove quotes + tweak bg image

--- src/blocks/intro-quote/edit.js
+++ src/blocks/intro-quote/edit.js
@@ -28,8 +28,8 @@ export default function Edit({ attributes, setAttributes }) {
 
 	const customStyle = {
 		...props.style,
-		backgroundImage: undefined,
-	}
+		backgroundImage: undefined
+	};
 
 	const defaultFormats = useRichtextDefaultFormats();
 	const allowedFormats = defaultFormats.filter(format => !DISABLED_FORMATS.includes(format.name)).map(format => format.name);
@@ -77,9 +77,9 @@ export default function Edit({ attributes, setAttributes }) {
 					</PanelRow>
 				</PanelBody>
 			</InspectorControls>
-			<div {...useBlockProps({style: customStyle})}>
+			<div {...useBlockProps({ style: customStyle })}>
+				<img className="evolutio-quote-bg" src={backgroundImage} alt="Evolutio logo" />
 				<div className="evolutio-quote-block">
-					<QuoteIcon className="evolutio-quote-svg evolutio-quote-svg-start" />
 					<RichText
 						tagName="p"
 						allowedFormats={allowedFormats}
@@ -88,36 +88,11 @@ export default function Edit({ attributes, setAttributes }) {
 						placeholder="Enter your quote..."
 						className="evolutio-quote-content"
 					/>
-					<QuoteIcon className="evolutio-quote-svg evolutio-quote-svg-end" />
-					<img className="evolutio-quote-bg" src={backgroundImage} alt="Evolutio logo" />
 				</div>
 				<div className="evolutio-quote-link">
 					<span className="evolutio-link">{linkText}</span>
 				</div>
 			</div>
 		</Fragment>
-	);
-}
-
-/**
- * @argument {{
- *     className: string
- * }} props
- */
-function QuoteIcon(props) {
-	return (
-		<svg
-			className={props.className}
-			xmlns="http://www.w3.org/2000/svg"
-			viewBox="10 22 36 10"
-			aria-hidden="true"
-		>
-			<circle cx="18.5" cy="31.5" r="5.5" />
-			<path d="M18.5 38a6.5 6.5 0 1 1 .02-13.02A6.5 6.5 0 0 1 18.5 38zm0-11a4.5 4.5 0 1 0 .01 9.01A4.5 4.5 0 0 0 18.5 27z" />
-			<circle cx="35.5" cy="31.5" r="5.5" />
-			<path
-				d="M35.5 38a6.5 6.5 0 1 1 .02-13.02A6.5 6.5 0 0 1 35.5 38zm0-11a4.5 4.5 0 1 0 .01 9.01A4.5 4.5 0 0 0 35.5 27zM13 32a1 1 0 0 1-1-1c0-7.72 6.28-14 14-14a1 1 0 1 1 0 2c-6.62 0-12 5.38-12 12a1 1 0 0 1-1 1z" />
-			<path d="M30 32a1 1 0 0 1-1-1c0-7.72 6.28-14 14-14a1 1 0 1 1 0 2c-6.62 0-12 5.38-12 12a1 1 0 0 1-1 1z" />
-		</svg>
 	);
 }
--- src/blocks/intro-quote/render.php
+++ src/blocks/intro-quote/render.php
@@ -6,41 +6,17 @@ $backgroundImage = $attributes['style']['background']['backgroundImage']['url'] 
 
 $containerAttributes = get_block_wrapper_attributes();
 
-
-if (!function_exists('render_quote_icon')) {
-    function render_quote_icon($class_name = '')
-    {
-        ob_start();
-?>
-        <svg
-            class="<?php echo esc_attr($class_name); ?>"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="10 22 36 10"
-            aria-hidden="true">
-            <circle cx="18.5" cy="31.5" r="5.5" />
-            <path d="M18.5 38a6.5 6.5 0 1 1 .02-13.02A6.5 6.5 0 0 1 18.5 38zm0-11a4.5 4.5 0 1 0 .01 9.01A4.5 4.5 0 0 0 18.5 27z" />
-            <circle cx="35.5" cy="31.5" r="5.5" />
-            <path d="M35.5 38a6.5 6.5 0 1 1 .02-13.02A6.5 6.5 0 0 1 35.5 38zm0-11a4.5 4.5 0 1 0 .01 9.01A4.5 4.5 0 0 0 35.5 27zM13 32a1 1 0 0 1-1-1c0-7.72 6.28-14 14-14a1 1 0 1 1 0 2c-6.62 0-12 5.38-12 12a1 1 0 0 1-1 1z" />
-            <path d="M30 32a1 1 0 0 1-1-1c0-7.72 6.28-14 14-14a1 1 0 1 1 0 2c-6.62 0-12 5.38-12 12a1 1 0 0 1-1 1z" />
-        </svg>
-<?php
-        return ob_get_clean();
-    }
-}
-
 ?>
 <div <?php echo $containerAttributes ?>>
-    <div class="evolutio-quote-block">
-        <?php echo render_quote_icon('evolutio-quote-svg evolutio-quote-svg-start'); ?>
-        <div class="evolutio-quote-content">
-            <?php echo $content; ?>
-        </div>
-        <?php echo render_quote_icon('evolutio-quote-svg evolutio-quote-svg-end'); ?>
-			<img class="evolutio-quote-bg" src="<?php echo esc_url($backgroundImage); ?>" alt="Evolutio logo">
-    </div>
-    <div class="evolutio-quote-link">
-        <a class="evolutio-link" href="<?php echo esc_url($link); ?>">
-            <?php echo esc_html($linkText); ?>
-        </a>
-    </div>
+	<img class="evolutio-quote-bg" src="<?php echo esc_url($backgroundImage); ?>" alt="Evolutio logo">
+	<div class="evolutio-quote-block">
+		<div class="evolutio-quote-content">
+			<?php echo $content; ?>
+		</div>
+	</div>
+	<div class="evolutio-quote-link">
+		<a class="evolutio-link" href="<?php echo esc_url($link); ?>">
+			<?php echo esc_html($linkText); ?>
+		</a>
+	</div>
 </div>
